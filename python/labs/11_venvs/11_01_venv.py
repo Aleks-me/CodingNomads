91d371ab5292568c6f55d92aae46c5d7ebccb4b5
Alex <aleks.kuznetsov.1989@gmail.com>
2020-06-17 11:06:43 +0300

    Completed venv labs.

--- python/labs/11_venvs/11_01_venv.py
+++ python/labs/11_venvs/11_01_venv.py
@@ -7,6 +7,19 @@ In your CodingNomads folder create a new folder. Inside of that folder:
 4. Freeze the installed packages to a requirements.txt file.
 5. Deactivate the virtual environment.
 6. Delete the virtual environment.
-7. Create a new virtual environment and install the packages from the requirements.txt file.
+7. Create a new virtual environment and install the packages from
+   the requirements.txt file.
 
 '''
+
+"""
+CLI commands:
+
+$ python -m venv env_1 && source env_1/bin/activate
+$ pip install django numpy pytest
+$ pip freeze > ../requirements.txt && deactivate
+$ rm -rf env_1
+$ python -m venv env_2 && source env_2/bin/activate
+$ pip install -r ../requirements.txt && deactivate
+
+"""
---
+++ python/labs/11_venvs/env_2/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alex_me/Documents/CodingNomads/python/labs/11_venvs/env_2/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
