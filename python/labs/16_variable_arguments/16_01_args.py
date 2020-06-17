1d9aa271ee416c8061b0c690c183f20e62b5893b
Alex <aleks.kuznetsov.1989@gmail.com>
2020-06-17 16:36:22 +0300

    Completed labs 16_

--- python/labs/16_variable_arguments/16_01_args.py
+++ python/labs/16_variable_arguments/16_01_args.py
@@ -2,3 +2,14 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+
+def like_eating(*args):
+    for food in args:
+        print(f"I like to eat {food}")
+
+
+like_eating("pizza", "soup", "bread", "steak")
+
+a_list = ["sausage", "cranberries", "tomatoes", "pilaf"]
+like_eating(*a_list)
--- python/labs/16_variable_arguments/16_02_kwargs.py
+++ python/labs/16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,16 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+
+def like_eating(**kwargs):
+    for suffix, food in kwargs.items():
+        print(f"I like to eat {suffix} {food}")
+
+
+like_eating(hot="pizza", chicken="soup", fresh="bread", well_done="steak")
+
+a_dict = {"beef": "sausage", "jam": "cranberries",
+          "yellow": "tomatoes", "Kazachstanian": "pilaf"}
+
+like_eating(**a_dict)
