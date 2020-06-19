244f9bd4c1e671082458522d6fdde6ed2a73f1a5
Alex <aleks.kuznetsov.1989@gmail.com>
2020-06-19 16:25:15 +0300

    Completed labs 17_

--- python/labs/17_decorators/17_01_wraps.py
+++ python/labs/17_decorators/17_01_wraps.py
@@ -2,3 +2,17 @@
 Write a decorator function that wraps text passed to it in a html <p> tag.
 
 '''
+
+
+def decorator(func):
+    def wrapper(msg):
+        return f"<p>{func(msg)}<p>"
+    return wrapper
+
+
+@decorator
+def texting(msg):
+    return msg
+
+
+print(texting("When you see it - you shit bricks!"))
--- python/labs/17_decorators/17_02_tags.py
+++ python/labs/17_decorators/17_02_tags.py
@@ -3,3 +3,19 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+
+
+def decorator_tag(tag):
+    def decorator(func):
+        def wrapper(msg):
+            return f"{tag}{func(msg)}{tag}"
+        return wrapper
+    return decorator
+
+
+@decorator_tag("<old_memes>")
+def texting(msg):
+    return msg
+
+
+print(texting("When you see it - you shit bricks!"))
